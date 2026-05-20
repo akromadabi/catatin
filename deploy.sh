ffd402e7845990406055ee473d68fc4e80939df2
akromadabi <akromadabi@gmail.com>
2026-05-20 12:33:57 +0700

    fix: update path to /www/wwwroot/siapp.in in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo "🚀 MEMULAI DEPLOYMENT CATAT-IN"
 echo "=========================================="
 
 # Pindah ke direktori aplikasi (sesuaikan jika path di aaPanel berbeda)
-cd /www/wwwroot/catatin
+cd /www/wwwroot/siapp.in
 
 echo "⬇️ 1. Menarik pembaruan terbaru dari GitHub..."
 git pull origin main
